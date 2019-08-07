4a97cdaabe93f11b2056d05a77468f64ac7c6f41
Riemannsa <manshalitest@gmail.com>
2019-08-07 15:20:49 -0400

    Initial commit

---
+++ Print sentence_word saved with variables.py
@@ -0,0 +1,14 @@
+a="All"
+b="work"
+c="and"
+d="no"
+e="play"
+f="makes"
+g="Jack"
+h="a"
+i="dull"
+j="boy"
+#print(a,"",b,"",c,"",d,"",e,"",f,"",g,"",h,"" ,i,"",j,".")#All  work  and  no  play  makes  Jack  a  dull  boy .
+#print(a+b+c+d+e+f+g+h+i+j+".") #AllworkandnoplaymakesJackadullboy.
+#print(a,b,c,d,e,f,g,h,i,j+".") #All work and no play makes Jack a dull boy.
+print(a,b,c,d,e,f,g,h,i,j,".")  #All work and no play makes Jack a dull boy .
