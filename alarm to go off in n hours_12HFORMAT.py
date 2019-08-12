f4972fbb77f17e6f561f2a969b3ed958e2bfb3a6
Riemannsa <manshalitest@gmail.com>
2019-08-12 16:38:35 -0400

    12 Aug-1

--- alarm to go off in n hours_12HFORMAT.py
+++ alarm to go off in n hours_12HFORMAT.py
@@ -1,14 +1,37 @@
-h = int (input("How many hours until the alarm goes off?")
-if h<0:
-         print ("Time only moves forward, please input a positive number:")
-else:
-    
-D =(h//24)
 
-         
-if H==0:
-        
-    print("The alarm go off in",D,"days at",HH,"PM")
+h=int(input("How many hours until the alarm goes off?"))
+
+while h<0:
+    h2=int(input("Time only moves forward, plase input a postive number:"))
+    if h2<0:
+        print ("No negative input accepted, program ends for repeatly invalid input.")
+        break
+    else:
+        h=h2
+        break
+
+while h==0:
+     print("The alarm goes off now.")
+     break
+    
+while h>0:
+    if h<=24:
+        D=0
+    else:
+        D=int(h//24)
+
+    hh=(h%12+2)%12
+
+    if ((h+2)//12)%2==0:
+        print("The alarm is going off in",D,"day at",hh,":00 PM.")
+    if ((h+2)//12)%2==1:
+        print("The alarm is going off in",D,"day at",hh,":00 AM.")
+    break
+
+
+    
    
-if H!=0:
-    print("The alarm go off in",D,"days at",HH,"AM")
+      
+   
+        
+
--- alarm to go off in n hours_24HFORMAT.py
+++ alarm to go off in n hours_24HFORMAT.py
@@ -1,9 +1,33 @@
+
 h=int(input("How many hours until the alarm goes off?"))
-if h>=0:
-    D=int(h//24)
+
+while h<0:
+    h2=int(input("Time only moves forward, plase input a postive number:"))
+    if h2<0:
+        print ("No negative input accepted, program ends for repeatly invalid input.")
+        break
+    else:
+        h=h2
+        break
+
+while h==0:
+     print("The alarm goes off now.")
+     break
+    
+while h>0:
+    if h<=24:
+        D=0
+    else:
+        D=int(h//24)
+        
     hh=(h%24+14)%24
     print("The alarm is going off in",D,"day at",hh,":00.")
-else:
-    input("Time only moves forward, plase input a postive number:")
+    break
+
+
+    
+   
+      
+   
         
 
